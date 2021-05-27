0e5f6fc4c12a01077f60c8c2ce785abc8d8f5c87
Magda Sz <pesahson@gmail.com>
2021-05-27 18:14:52 +0200

    change mobile menu

--- insure-landing-page-master/script.js
+++ insure-landing-page-master/script.js
@@ -1,7 +1,12 @@
 const navigation = document.querySelector("nav");
+const mobileNav = document.querySelector('.nav__list-mobile')
+
 const navToggle = navigation.querySelector(".nav__btns");
 
 
 navToggle.addEventListener("click", () => {
   navigation.classList.toggle("show-mobile-nav");
+  mobileNav.classList.toggle('show-mobile-nav')
 });
+
+
